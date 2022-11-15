a6c82e056183fd4115a6b1378d834141aa5233b5
kf68et14 <quan97.lequan@gmail.com>
2022-11-15 23:01:28 +0700

    completed

--- src/althorithm1/BinarySearchTest.java
+++ src/althorithm1/BinarySearchTest.java
@@ -1,23 +1,23 @@
-package althorithm1;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
-
-class BinarySearchTest {
-
-    private BinarySearch test = new BinarySearch();
-    private BinarySearch2 test2 = new BinarySearch2();
-    int[] numbers = {-1,0,3,5,9,12};
-    int target = 9;
-    int result = 4;
-    @Test
-    void search() {
-        assertEquals(test.search(numbers, target), result);
-    }
-
-    @Test
-    void search2() {
-        assertEquals(test2.search2(numbers, target), result);
-    }
-}
+//package althorithm1;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//class BinarySearchTest {
+//
+//    private BinarySearch test = new BinarySearch();
+//    private BinarySearch2 test2 = new BinarySearch2();
+//    int[] numbers = {-1,0,3,5,9,12};
+//    int target = 9;
+//    int result = 4;
+//    @Test
+//    void search() {
+//        assertEquals(test.search(numbers, target), result);
+//    }
+//
+//    @Test
+//    void search2() {
+//        assertEquals(test2.search2(numbers, target), result);
+//    }
+//}
--- src/quan/NextGreaterElementI.java
+++ src/quan/NextGreaterElementI.java
@@ -1,18 +1,36 @@
 package quan;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class NextGreaterElementI {
     public static void main(String[] args) {
-
-    }
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<Integer>();
-        for (int i = 0; i < nums2.length; i++){
-            stack.push(nums2[i]);
+        int[]nums1={4,1,2};
+        int[]nums2={1,3,4,2};
+        int[]ans=nextGreaterElement(nums1,nums2);
+        for(int i:ans){
+            System.out.print(i+" ");
         }
-        for (int i = 0; i < nums1.length; i++){
-
-        }
-        return 0;
     }
-}
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>stack=new Stack<Integer>();
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        int[]ans=new int[nums1.length];
+        for(int i:nums2){
+            while(!stack.isEmpty()){
+                if(stack.peek()<i){
+                    map.put(stack.peek(),i);
+                    stack.pop();
+                }else break;
+            }
+            stack.push(i);
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i]))
+                ans[i]=map.get(nums1[i]);
+            else ans[i]=-1;
+        }
+        return ans;
+    }
+    }
+
