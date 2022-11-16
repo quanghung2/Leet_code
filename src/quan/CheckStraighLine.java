5df1d8c654035214ec4fb47195e2509ad1f2f6d1
kf68et14 <quan97.lequan@gmail.com>
2022-11-16 22:57:10 +0700

    done

---
+++ src/quan/CheckStraighLine.java
@@ -0,0 +1,25 @@
+package quan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CheckStraighLine {
+    private static boolean checkRadian (int[] A, int[] B, int[] C){
+        return (A[0] - B[0]) * (A[1] - C[1]) == (A[0] - C[0]) * (A[1] - B[1]);
+    }
+    public static boolean checkStraightLine(int[][] coordinates){
+        for(int[] A: coordinates){
+            for(int[] B: coordinates){
+                if(checkRadian(A, B, coordinates[0])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] coordinates = {{1,1},{2,2},{3,4},{4,5},{5,6},{7,7}};
+        System.out.println(checkStraightLine(coordinates));
+    }
+}
