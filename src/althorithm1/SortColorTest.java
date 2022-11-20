43dd9b81e67a35037ea9cb58cf2af2eb2b31365a
kf68et14 <quan97.lequan@gmail.com>
2022-11-20 15:30:42 +0700

    done

--- src/althorithm1/SortColorTest.java
+++ src/althorithm1/SortColorTest.java
@@ -1,7 +1,9 @@
 package althorithm1;
-import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SortColorTest {
 
---
+++ src/quan/PascalTriangle.java
@@ -0,0 +1,33 @@
+package quan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public static List<List<Integer>> generate(int numRows){
+        List<List<Integer>> result = new ArrayList<>();
+
+        List<Integer> pre = new ArrayList<>();
+        pre.add(1);
+
+        result.add(pre);
+
+        for (int i = 1; i < numRows; i++ ){
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for (int j = 1; j < i; j++){
+                temp.add(pre.get(j) + pre.get(j-1));
+            }
+            temp.add(1);
+            pre = temp;
+            result.add(pre);
+        }
+        return result;
+    }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(4));
+    }
+}
+
