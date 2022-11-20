0f0012fd1cb9b00dccb07ade5d72264971f05fae
quang hung <tr.hungquang@gmail.com>
2022-11-20 22:37:22 +0700

    push codee

--- src/althorithm1/IntersectionArrayTest.java
+++ src/althorithm1/IntersectionArrayTest.java
@@ -1,16 +1,17 @@
-//package althorithm1;
-//
-//
-//
-//import static org.junit.Assert.assertArrayEquals;
-//
-//class IntersectionArrayTest {
-//    IntersectionArray test = new IntersectionArray();
-//    int[] arr1 = {1, 2, 2, 1};
-//    int[] arr2 = {2, 2};
-//    int[] res = {2, 2};
-//    @Test
-//    void intersection() {
-//        assertArrayEquals(test.intersection(arr1, arr2), res);
-//    }
-//}
+package althorithm1;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+class IntersectionArrayTest {
+    IntersectionArray test = new IntersectionArray();
+    int[] arr1 = {1, 2, 2, 1};
+    int[] arr2 = {2, 2};
+    int[] res = {2, 2};
+    @Test
+    void intersection() {
+        assertArrayEquals(test.intersection(arr1, arr2), res);
+    }
+}
